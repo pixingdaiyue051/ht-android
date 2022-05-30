2d09a1ed85435e50c4d34fbbff6abce379f5d455
hexk <hexiaokun@hzjiuguan.net>
2022-05-30 09:24:31 +0800

    测试https和http请求

--- bar/src/main/java/com/tequeno/bar/okhttp/HttpActivity.java
+++ bar/src/main/java/com/tequeno/bar/okhttp/HttpActivity.java
@@ -1,21 +1,26 @@
 package com.tequeno.bar.okhttp;
 
 import android.os.Bundle;
-import android.text.TextUtils;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.tequeno.bar.R;
 
+/**
+ * 发送http请求必须是非主线程 修改ui必须使用主线程
+ * 因此不能在异步的任务里处理对ui的相关修改  需要在ui线程中提前定义一个handler
+ */
 public class HttpActivity extends AppCompatActivity {
 
     private final static String TAG = "HttpActivity";
     private HttpUtil httpUtil;
-    private EditText ed;
+    private TextView tv;
+    private Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,23 +28,43 @@ public class HttpActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_http);
 
-        ed = findViewById(R.id.h_ed);
-        Button btnOk = findViewById(R.id.btn_ok);
+        tv = findViewById(R.id.h_tv);
+        Button btnCs = findViewById(R.id.btn_cs);
+        Button btnProd = findViewById(R.id.btn_prod);
+        Button btnHttps = findViewById(R.id.btn_https);
 
-        btnOk.setOnClickListener(this::ok);
+        btnCs.setOnClickListener(this::cs);
+        btnProd.setOnClickListener(this::prod);
+        btnHttps.setOnClickListener(this::https);
 
         httpUtil = HttpUtil.getInstance();
-
+        handler = new Handler();
     }
 
-    private void ok(View view) {
-        String url = "http://qinshitong.work:8082/admin/tenantUser/login";
+    private void cs(View view) {
+        String url = "http://qinshitong.work:8082/server/demo/time";
 
 //        httpUtil.get(url, msg -> Log.d(TAG, "ok: " + msg));
 
-        httpUtil.post(url, "{\"password\":\"123456\",\"username\":\"abc\"}", msg -> {
-            Log.d(TAG, "ok: " + msg);
-            httpUtil.post("http://qinshitong.work:8082/admin/tenantUser/list", "", msg1 -> Log.d(TAG, "ok: " + msg1));
+        httpUtil.post(url, msg -> {
+            Log.d(TAG, "cs: " + msg);
+            handler.post(() -> tv.setText(msg));
+        });
+    }
+
+    private void prod(View view) {
+        String url = "http://jiansuotong.top:8888/nmgjb/demo/time";
+        httpUtil.get(url, msg -> {
+            Log.d(TAG, "prod: " + msg);
+            handler.post(() -> tv.setText(msg));
+        });
+    }
+
+    private void https(View view) {
+        String url = "https://jiansuotong.top/app/demo/one";
+        httpUtil.get(url, msg -> {
+            Log.d(TAG, "prod: " + msg);
+            handler.post(() -> tv.setText(msg));
         });
     }
 }
